40fe4051c4804430f9fe700c4e899d852886fdbf
Eduardo Antuña Díez <eduadiez@imagine800.es>
2017-10-30 18:28:47 -0500

    Build fixing

--- src/components/VideoWallOfFameHeader.js
+++ src/components/VideoWallOfFameHeader.js
@@ -1,11 +1,71 @@
 import React, { Component } from 'react'
+import firebase from 'firebase';
+
 //import { Link } from 'react-router-dom'
 
 class VideoWallOfFameHeader extends Component {
+
+    constructor() {
+        super()
+
+       var _ref = firebase.database().ref("GVWOF_v2/").orderByKey().limitToLast(1)
+
+        this.state = {
+            databaseRef: _ref,
+            media:[],
+            src:""
+        }
+
+        _ref.on('value', this.gotData, (err) => { console.log(err) });
+
+    }
+
+    //get the data from the firebase and push them out
+    gotData = (data) => {
+        var newMedia = this.state.media
+        const mediadata = data.val();
+        console.log(mediadata)
+
+        if (!mediadata)
+            return
+
+        var keys = Object.keys(mediadata)
+
+        for (var i = 0; i < keys.length; i++) {
+            const k = keys[i];
+
+            newMedia.push({
+                id: keys[i],
+                title: mediadata[k].title,
+                src: mediadata[k].src,
+                type: mediadata[k].type,
+                ipfs: mediadata[k].ipfs,
+                description: mediadata[k].description,
+                timestamp: mediadata[k].timestamp,
+                wall: mediadata[k].wall,
+                week: mediadata[k].week,
+            });
+
+        }
+
+
+        this.setState({
+            src: newMedia[0].src
+        });
+
+
+
+    }
+
+
     render() {
         return (
             <div id="wall-of-fame-header">
+                <div>
+                    <video className="VideoHeader" autoPlay muted loop src={this.state.src} />
+                </div>
                 <div className="vertical-align">
+
                     <center>
                         <h3>Together we will save the world!</h3>
                         <div>
