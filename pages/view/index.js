84d1736571cbd157075b23cf4c11342e86192855
deam <deamme@users.noreply.github.com>
2018-10-11 22:43:45 +0200

    Remove url og meta tag

--- pages/view/index.js
+++ pages/view/index.js
@@ -112,7 +112,6 @@ class View extends Component {
             <Head>
               <meta property="og:title" content={media.title} />
               <meta property="og:type" content="video.other" />
-              <meta property="og:url" content={media.src} />
               <meta property="og:description" content={media.description} />
             </Head>
             <Link route="/">
